ec412a4da2a0c835dfba53d00800b087b4e2a571
BuildTools <unconfigured@null.spigotmc.org>
2020-02-26 19:17:45 +0000

    Text files converted to ints and printed
    Added text files into the program and made it so they are converted to ints and able to be printed - need to add a feature where you can select which array to print

--- Assignment_1_Algorithms/Assignment_1_Algorithms/Program.cs
+++ Assignment_1_Algorithms/Assignment_1_Algorithms/Program.cs
@@ -1,28 +1,74 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.IO;
 
 namespace Algorithhms_and_complexity_asignment_1
 {
     class Program
     {
+        static List<Arrays> Net_1_List = new List<Arrays>();
+        static List<Arrays> Net_2_List = new List<Arrays>();
+        static List<Arrays> Net_3_List = new List<Arrays>();
         static void Main(string[] args)
         {
+            Console.WriteLine($"Please eneter which array you would like to select\nThe arrays you are able to select are\n'Net_1'");
+            string SelectArracy = Console.ReadLine();
 
+            if (SelectArracy == "Net_1")
+            {
+                string[] lines = ReadFiles();
+            }
+            else
+            {
+                Console.WriteLine("You have not enetered a correct input. Please exit and enter a correct input");
+            }
         }
 
 
-        static void ReadFile() //  Read the files and convert into an array of integers
+        public static string[] ReadFiles() //  Read the files and convert into an array of integers
         {
+            try
+            {
+                string[] Net1 = File.ReadAllLines("Net_1_256.txt");
+                string[] Net2 = File.ReadAllLines("Net_2_256.txt");
+                string[] Net3 = File.ReadAllLines("Net_3_256.txt");
 
-            /*  string[] Net1 = File.ReadAllLines("Net_1_256.txt");
-              foreach (string s in Net1)
-              {
-                  Console.WriteLine(s);
-              }*/
+                int count = 0;
+                foreach (string s in Net1)
+                {
+                    Arrays a = new Arrays(int.Parse(Net1[count]), int.Parse(Net2[count]), int.Parse(Net3[count]));
+                    Net_1_List.Add(a);
+                    Net_2_List.Add(a);
+                    Net_3_List.Add(a);
+                    Console.WriteLine(Net_1_List[count].Net_1);
+                    Console.WriteLine(Net_2_List[count].Net_2);
+                    Console.WriteLine(Net_3_List[count].Net_3);
+                    count++;
+                }
+                Console.ReadKey();
+                return null;
+            }
+            catch(FileNotFoundException)
+            {
+                string[] temp = { "Net_1_256.txt not found " };
+                return temp;
+            }
+        }
 
-            string text = File.ReadAllText("Net_1_256.txt");
-
-
+    }
+    class Arrays
+    {
+        public int Net_1 { get; set; }
+        public int Net_2 { get; set; }
+        public int Net_3 { get; set; }
+        public Arrays(int Net_1_, int Net_2_, int Net_3_)
+        {
+            Net_1 = Net_1_;
+            Net_2 = Net_2_;
+            Net_3 = Net_3_;
         }
     }
 }

01ad2dd9569a5f6f17d262f635a3ed665b20f0a1
BuildTools <unconfigured@null.spigotmc.org>
2020-02-26 17:48:30 +0000

    File Setup

---
+++ Assignment_1_Algorithms/Assignment_1_Algorithms/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Algorithhms_and_complexity_asignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+
+        static void ReadFile() //  Read the files and convert into an array of integers
+        {
+
+            /*  string[] Net1 = File.ReadAllLines("Net_1_256.txt");
+              foreach (string s in Net1)
+              {
+                  Console.WriteLine(s);
+              }*/
+
+            string text = File.ReadAllText("Net_1_256.txt");
+
+
+        }
+    }
+}
